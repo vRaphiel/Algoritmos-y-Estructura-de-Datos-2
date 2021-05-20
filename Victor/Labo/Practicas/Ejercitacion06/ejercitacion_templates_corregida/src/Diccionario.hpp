3f75e11538d654550440b8c4edf142ac101c1b80
Victor Asmad <vraphieljr@gmail.com>
2021-05-20 11:58:13 -0300

    FEAT - Labo06 - Templates

--- Victor/Labo/Practicas/Ejercitacion06/ejercitacion_templates_corregida/src/Diccionario.hpp
+++ Victor/Labo/Practicas/Ejercitacion06/ejercitacion_templates_corregida/src/Diccionario.hpp
@@ -9,8 +9,7 @@ class Diccionario{
         void definir(Clave k, Valor v);
         bool def(Clave k) const;
         Valor obtener(Clave k) const;
-        std::vector<Clave> claves();
-        Valor menor() const;
+        std::vector<Clave> claves() const;
     private:
         struct Asociacion{
             Asociacion(Clave k, Valor v);
@@ -58,19 +57,23 @@ Valor Diccionario<Clave, Valor>::obtener(Clave k) const {
 }
 
 template<class Clave, class Valor>
-Clave Diccionario<Clave, Valor>::menor() const {
-
-}
-
-template<class Clave, class Valor>
-std::vector<Clave> Diccionario<Clave, Valor>::claves() {
+std::vector<Clave> Diccionario<Clave, Valor>::claves() const {
     std::vector<Clave> v1(_asociaciones.size(), 0);
+    std::vector<Clave> v2;
     for(int i = 0; i< _asociaciones.size(); i++){
         v1[i] = _asociaciones[i].clave;
     }
-
-    for(int i = 0; i<v1.size(); i++){
-
+    while(v1.size() > 0){
+        Clave menor = v1[0];
+        int pos = 0;
+        for(int i = 0; i < v1.size(); i++){
+            if(v1[i] < menor){
+                menor = v1[i];
+                pos = i;
+            }
+        }
+        v2.push_back(menor);
+        v1.erase(v1.begin() + pos);
     }
-    return v1;
+    return v2;
 }
--- Victor/Labo/Practicas/Ejercitacion06/ejercitacion_templates_corregida/src/Multiconjunto.hpp
+++ Victor/Labo/Practicas/Ejercitacion06/ejercitacion_templates_corregida/src/Multiconjunto.hpp
@@ -5,7 +5,9 @@ class Multiconjunto{
 public:
     Multiconjunto();
     void agregar(T x);
-    int ocurrencias(T x);
+    int ocurrencias(T x) const;
+    int cardinal() const;
+    bool operator<=(Multiconjunto<T> otro) const;
 private:
     Diccionario<T, int> _dicc;
 };
@@ -24,10 +26,47 @@ void Multiconjunto<T>::agregar(T x) {
 }
 
 template<class T>
-int Multiconjunto<T>::ocurrencias(T x) {
+int Multiconjunto<T>::ocurrencias(T x) const {
     if(_dicc.def(x)){
         return _dicc.obtener(x);
     }else{
         return 0;
     }
+}
+
+template<class T>
+int Multiconjunto<T>::cardinal() const{
+    return this->_dicc.claves().size();
+}
+
+template<class T>
+bool Multiconjunto<T>::operator<=(Multiconjunto<T> otro) const {
+    // Vemos si esta uno dentro de otro
+    // Llamo a mi conjunto con this, y al otro como otro
+    bool estaIncluido = true;
+    // Valido si los cardinales estan en rango
+    // Si este multiconjunto esta en otro su cardinal debe ser menor al del otro
+    int thisCardinal = this->cardinal();
+    int thatCardinal = otro.cardinal();
+
+    if(thisCardinal <= thatCardinal){
+
+        for(int i = 0; i<this->cardinal(); i++){
+
+            T xFirst = this->_dicc.claves()[i];
+            T xSecond = otro._dicc.claves()[i];
+
+            int xOcurrenciasF = ocurrencias(xFirst);
+            int xOcurrenciasS = otro.ocurrencias(xSecond);
+
+            if(xOcurrenciasF > xOcurrenciasS){
+                estaIncluido = false;
+            }
+
+        }
+
+    }else{
+        estaIncluido = false;
+    }
+    return estaIncluido;
 }
